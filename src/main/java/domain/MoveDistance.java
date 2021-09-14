997a78b9a467172122978d6eb58cf2f20787c903
kdh85 <modudamba@naver.com>
2021-09-14 13:38:26 +0900

    feat : 자동차경주 거리 객체 생성 거리값을 담당하는 객체 생성.

---
+++ src/main/java/domain/MoveDistance.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.util.Objects;
+
+public class MoveDistance {
+
+  private int distance;
+
+  public MoveDistance() {
+    distance = 0;
+  }
+
+  public void move() {
+    distance += 1;
+  }
+
+  public int distance(){
+    return distance;
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final MoveDistance that = (MoveDistance) o;
+    return distance == that.distance;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(distance);
+  }
+
+  @Override
+  public String toString() {
+    return "MoveDistance{" +
+        "distance=" + distance +
+        '}';
+  }
+
+}
