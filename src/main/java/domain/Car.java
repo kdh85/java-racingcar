ca25669e63153712b9fcf9900f66c844c7e3346e
kdh85 <modudamba@naver.com>
2021-09-14 17:50:01 +0900

    feat : 자동차경주 자동차 객체 입력값 검증 추가 입력받는 숫자의 값이 0~9만 허용되도록 검증 로직 추가.

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,6 +5,10 @@ import java.util.Objects;
 public class Car {
 
   private static final int MOVE_LIMIT_NUMBER = 4;
+  private static final int MIN_NUMBER = 0;
+  private static final int MAX_NUMBER = 9;
+  private static final String MSG_ERROR_LIMIT_NUMBER = "입력받는 난수값은 0부터 9까지입니다.";
+
   private final Name carName;
 
   private final MoveDistance moveDistance;
@@ -23,8 +27,16 @@ public class Car {
   }
 
   public void moveThaCar(final int number) {
-    if(number >= MOVE_LIMIT_NUMBER){
+    validationNumber(number);
+
+    if (number >= MOVE_LIMIT_NUMBER) {
       moveDistance.move();
+    }
+  }
+
+  private void validationNumber(final int number) {
+    if (number < MIN_NUMBER || number > MAX_NUMBER) {
+      throw new IllegalArgumentException(MSG_ERROR_LIMIT_NUMBER);
     }
   }
 
