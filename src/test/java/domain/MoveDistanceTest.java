a57d81a80a78ff26ba5f52073a70bb5c39b6b561
kdh85 <modudamba@naver.com>
2021-09-14 13:38:56 +0900

    test : 자동차경주 거리 객체 테스트코드 생성 기본거리가 0인 객체 생성 테스트 코드 작성. 한번 이동했을때의 거리값 변화에 대한 테스트코드 작성.

---
+++ src/test/java/domain/MoveDistanceTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MoveDistanceTest {
+
+  private MoveDistance moveDistance;
+
+  @BeforeEach
+  void setUp() {
+    moveDistance = new MoveDistance();
+  }
+
+  @DisplayName("기본 거리 0으로 객체 생성이 된다.")
+  @Test
+  void defaultDistance() {
+    assertThat(moveDistance.distance()).isEqualTo(0);
+  }
+
+  @DisplayName("이동을 수행하면 거리1이 증가한다.")
+  @Test
+  void move() {
+    moveDistance.move();
+    assertThat(moveDistance.distance()).isEqualTo(1);
+  }
+}
