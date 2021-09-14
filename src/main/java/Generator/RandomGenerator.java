1bec4c69ebfecb19f3268b3c645aa1c904df0cb1
kdh85 <modudamba@naver.com>
2021-09-14 23:11:55 +0900

    feat : 자동차경주 0~9랜덤 넘버 생성 0~9중 자동으로 번호를 생성하여 반환하는 클래스 메소드 생성

---
+++ src/main/java/Generator/RandomGenerator.java
@@ -0,0 +1,15 @@
+package Generator;
+
+import java.util.Random;
+
+public class RandomGenerator {
+
+  private static final int MAX_NUMBER = 10;
+  private static final int LESS_NUMBER = 1;
+
+  private static Random random = new Random();
+
+  public static int createNumber() {
+    return random.nextInt(MAX_NUMBER) * LESS_NUMBER;
+  }
+}
