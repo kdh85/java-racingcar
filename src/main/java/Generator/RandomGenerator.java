21ca2759156061fd13310b33aa2e916f75792a1e
kdh85 <modudamba@naver.com>
2021-09-17 10:05:40 +0900

    refactor : 자동차경주 랜덤수 생성 final 추가 변수 선언에 누락된 final 추가.

--- src/main/java/Generator/RandomGenerator.java
+++ src/main/java/Generator/RandomGenerator.java
@@ -7,7 +7,7 @@ public class RandomGenerator {
   private static final int MAX_NUMBER = 10;
   private static final int LESS_NUMBER = 1;
 
-  private static Random random = new Random();
+  private static final Random random = new Random();
 
   public static int createNumber() {
     return random.nextInt(MAX_NUMBER) * LESS_NUMBER;
