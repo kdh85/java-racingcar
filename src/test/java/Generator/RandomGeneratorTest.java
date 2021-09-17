c285ede273259c4c231cda3c2f01b69eedf2a1f6
kdh85 <modudamba@naver.com>
2021-09-17 14:16:35 +0900

    test : 자동차경주 랜덤번호 객체 메소드 테스트코드 추가 랜덤번호를 실행 횟수 만큼 생성하여 반환하는 메소드 테스트코드 추가.

--- src/test/java/Generator/RandomGeneratorTest.java
+++ src/test/java/Generator/RandomGeneratorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RandomGeneratorTest {
 
@@ -11,5 +13,12 @@ class RandomGeneratorTest {
   @Test
   void createNumber() {
     assertThat(RandomGenerator.createNumber()).isGreaterThan(-1).isLessThan(10);
+  }
+
+  @DisplayName("0~9사이로 랜덤한 숫자를 입력받은 수 만큼 생성되는지 확인한다.")
+  @ParameterizedTest
+  @ValueSource(ints = {1,3,5})
+  void createNumbers(int count) {
+    assertThat(RandomGenerator.createNumbers(count).size()).isEqualTo(count);
   }
 }
