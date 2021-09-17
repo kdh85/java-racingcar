5fdc486f2f638e2b76a638cee9db51b925384468
kdh85 <modudamba@naver.com>
2021-09-17 13:02:38 +0900

    test : 자동차경주 거리객체 이동거리관련 메소드 테스트코드 추가 거리 객체간 비교를 통해서 최대 거리인지 확인하는 메소드 테스트코드 추가. 거리 객체간 비교를 통해서 같은 거리인지 확인하는 메소드 테스트코드 추가.

--- src/test/java/domain/MoveDistanceTest.java
+++ src/test/java/domain/MoveDistanceTest.java
@@ -27,4 +27,19 @@ class MoveDistanceTest {
     moveDistance.move();
     assertThat(moveDistance.distance()).isEqualTo(1);
   }
+
+  @DisplayName("이동거리 최대값을 구한다.")
+  @Test
+  void isMaxDistance() {
+    MoveDistance target = new MoveDistance();
+    target.move();
+    assertThat(moveDistance.isMaxDistance(target)).isTrue();
+  }
+
+  @DisplayName("이동거리가 같은지 확인한다.")
+  @Test
+  void isSameDistance() {
+    MoveDistance target = new MoveDistance();
+    assertThat(moveDistance.isSameDistance(target)).isTrue();
+  }
 }
