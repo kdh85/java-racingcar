9b5638f7d95c2b58f0ccbc369c95011baf176751
kdh85 <modudamba@naver.com>
2021-09-17 10:15:27 +0900

    feat : 자동차경주 게임실행 main 클래스 생성 자동차 경주를 실행할 main 클래스 생성

---
+++ src/main/java/RacingMain.java
@@ -0,0 +1,18 @@
+import domain.Cars;
+import domain.Racing;
+import view.InsertView;
+import view.ResultView;
+
+public class RacingMain {
+
+  public static void main(String[] args) {
+
+    Racing racing = new Racing(Cars.createCars(InsertView.names()), InsertView.round());
+
+    ResultView.printResultTitle();
+
+    while (racing.isFinish()) {
+      ResultView.printResult(racing.runningResult());
+    }
+  }
+}
