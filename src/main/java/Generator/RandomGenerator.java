cf932096b18f2e195fe3b644321937fb29e597fc
kdh85 <modudamba@naver.com>
2021-09-17 14:16:21 +0900

    refactor : 자동차경주 랜덤번호 객체 메소드 추가 랜덤번호를 실행 횟수 만큼 생성하여 반환하는 메소드 추가.

--- src/main/java/Generator/RandomGenerator.java
+++ src/main/java/Generator/RandomGenerator.java
@@ -1,15 +1,25 @@
 package Generator;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class RandomGenerator {
 
   private static final int MAX_NUMBER = 10;
   private static final int LESS_NUMBER = 1;
+  private static final int START_INCLUSIVE = 0;
 
   private static final Random random = new Random();
 
   public static int createNumber() {
     return random.nextInt(MAX_NUMBER) * LESS_NUMBER;
+  }
+
+  public static List<Integer> createNumbers(final int count) {
+    return IntStream.range(START_INCLUSIVE, count)
+        .mapToObj(i -> random.nextInt(MAX_NUMBER) * LESS_NUMBER)
+        .collect(Collectors.toList());
   }
 }
