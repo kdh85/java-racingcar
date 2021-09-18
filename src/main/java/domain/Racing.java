328d932d8c4b8967faf62dc8f6a9d532728d73bd
kdh85 <modudamba@naver.com>
2021-09-18 21:13:50 +0900

    refactor : 자동차경주 우승자 추출 메소드 추가 자동차 경주의 우승자들의 정보를 문자열 타입으로 반환하는 메소드 추가.

--- src/main/java/domain/Racing.java
+++ src/main/java/domain/Racing.java
@@ -1,9 +1,13 @@
 package domain;
 
+import Generator.RandomGenerator;
 import dto.RacingResultDto;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Racing {
+
+  private static final String WINNER_SEPARATOR = ", ";
 
   private final int round;
 
@@ -18,10 +22,17 @@ public class Racing {
 
   public List<RacingResultDto> runningResult() {
     currentRound++;
-    return cars.movingCars().racingResults();
+    return cars.movingCars(RandomGenerator.createNumbers(round)).racingResults();
   }
 
   public boolean isFinish() {
     return round != this.currentRound;
+  }
+
+  public String winners() {
+    return cars.findWinner()
+        .stream()
+        .map(Car::carName)
+        .collect(Collectors.joining(WINNER_SEPARATOR));
   }
 }
